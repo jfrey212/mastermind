81a4d1bbbd4f4b1f749994622cca23cf97c9a942
jfrey212 <eojyerf@gmail.com>
2022-03-27 21:12:38 -0500

    Initial commit - created project files and initial classes

---
+++ mastermind_classes.rb
@@ -0,0 +1,24 @@
+class Game
+  def initialize
+  end
+end
+
+class Board
+  def initialize(secret_code)
+    @secret_code = secret_code
+    @guess = []
+  end
+  def display
+  end
+
+end
+
+class HumanPlayer
+  def initialize
+  end
+end
+
+class ComputerPlayer
+  def initialize
+  end
+end
